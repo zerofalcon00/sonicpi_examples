908d70bccd6aa264a8f51666e4b67ab740b499a6
Josh <mcnett88@gmail.com>
2016-08-01 15:04:29 -0400

    makeing_a_sandwhich

---
+++ makeing_a_sandwhich.rb
@@ -0,0 +1,22 @@
+#making sandwhich
+
+#Type of bread
+
+def get_bread()
+  sample(:ambi_choir)
+  sleep(1)
+end
+
+def get_mayo()
+  sample(:ambi_piano)
+  sleep(1)
+end
+
+def get_lettuce
+  sample(:bass_hard_c)
+  sleep(1)
+end
+
+get_bread()
+get_mayo()
+get_lettuce()
